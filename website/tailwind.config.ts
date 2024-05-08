9f6ebd3fa7b8daf813632d53c5fb3dc1a9a7f2cf
pheralb <pablohdez4340@gmail.com>
2024-05-08 09:37:15 +0100

    chore: add ``luxe`` custom animations

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -35,10 +35,22 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        flip: {
+          to: {
+            transform: 'rotate(360deg)',
+          },
+        },
+        rotate: {
+          to: {
+            transform: 'rotate(90deg)',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        flip: 'flip 6s infinite steps(2, end)',
+        rotate: 'rotate 3s linear infinite both',
       },
     },
   },
