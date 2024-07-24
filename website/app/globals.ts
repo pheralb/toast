af028105c570e8fa4330e71f12408d504f69a5b2
pheralb <pablohdez4340@gmail.com>
2024-07-24 10:14:42 +0100

    docs: update global site name, delete ``@``

--- website/app/globals.ts
+++ website/app/globals.ts
@@ -1,4 +1,4 @@
-export const siteTitle = '@pheralb/toast';
+export const siteTitle = 'pheralb/toast';
 export const eslogan = 'An accessible and beautiful toast library for React';
 export const siteUrl = 'https://toast.pheralb.dev';
 export const siteUrlImages = `${siteUrl}/images`;
