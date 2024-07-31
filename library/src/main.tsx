a2f4ccd5e170c7d356da0b7664f2993c712ff8a0
pheralb <pablohdez4340@gmail.com>
2024-07-31 22:18:36 +0100

    feat: update toast types and export additional toast-related types

--- library/src/main.tsx
+++ library/src/main.tsx
@@ -1,7 +1,9 @@
 export { toast } from './components/toast-functions';
 export { Toaster } from './components/toaster';
 export type {
+  Position as ToastPosition,
   ToastProps as ToastProperties,
   ToasterProperties,
+  Variant as ToastVariant,
   Theme as ToastTheme,
 } from './types/toast.types';
