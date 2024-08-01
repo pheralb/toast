970c13a786dfa4ceb2927e6d06f006e84a77c86b
pheralb <pablohdez4340@gmail.com>
2024-08-01 11:45:07 +0100

    fix: update toast functions to return data properties + update types for toast functions

--- library/src/components/toast-functions.ts
+++ library/src/components/toast-functions.ts
@@ -1,20 +1,33 @@
-import { ToastPropsWithVariant } from '../types/toast.types';
+import type { ToastPropsWithVariant } from '../types/toast.types';
 import { openToast } from './toaster';
 
-export const toast = {
+interface ToastFunctions {
+  default: (data: ToastPropsWithVariant) => ToastPropsWithVariant;
+  success: (data: ToastPropsWithVariant) => ToastPropsWithVariant;
+  error: (data: ToastPropsWithVariant) => ToastPropsWithVariant;
+  warning: (data: ToastPropsWithVariant) => ToastPropsWithVariant;
+  info: (data: ToastPropsWithVariant) => ToastPropsWithVariant;
+}
+
+export const toast: ToastFunctions = {
   default: (data: ToastPropsWithVariant) => {
     openToast({ ...data });
+    return data;
   },
   success: (data: ToastPropsWithVariant) => {
     openToast({ ...data, variant: 'success' });
+    return data;
   },
   error: (data: ToastPropsWithVariant) => {
     openToast({ ...data, variant: 'error' });
+    return data;
   },
   warning: (data: ToastPropsWithVariant) => {
     openToast({ ...data, variant: 'warning' });
+    return data;
   },
   info: (data: ToastPropsWithVariant) => {
     openToast({ ...data, variant: 'info' });
+    return data;
   },
 };
