2e6a441de1e1f5f6f42f91b331123b9301de0e69
pheralb <pablohdez4340@gmail.com>
2024-08-02 14:53:37 +0100

    feat: update ogImage in globals.ts

--- docs/src/globals.ts
+++ docs/src/globals.ts
@@ -1,4 +1,3 @@
 export const siteTitle = 'pheralb/toast';
 export const eslogan = 'An accessible and beautiful toast library for React';
-export const siteUrl = 'https://toast.pheralb.dev';
-export const siteUrlImages = `${siteUrl}/images`;
+export const ogImage = '/images/logo.png';
